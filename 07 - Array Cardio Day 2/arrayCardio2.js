1c542a87b72d1df4a9523cf81b7d3b8ded48727a
Eugene Theriault <hybridbassist@gmail.com>
2021-01-30 13:10:29 -0800

    Add linting and complete 14

--- 07 - Array Cardio Day 2/arrayCardio2.js
+++ 07 - Array Cardio Day 2/arrayCardio2.js
@@ -5,21 +5,21 @@
 function oneOver19(people) {
   let currentYear = new Date("January 23, 2021").getFullYear();
   return people.some(person => (currentYear - person.year) >= 19);
-};
+}
 
 function allOver19(people) {
   let currentYear = new Date("January 23, 2021").getFullYear();
   return people.every(person => (currentYear - person.year) >= 19);
-};
+}
 
 function findById(comments, id) {
   return comments.find(comment => comment.id === id);
-};
+}
 
 function removeById(comments, id) {
   let index = comments.findIndex(comments => comments.id === id);
   comments.splice(index, 1);
-};
+}
 
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
